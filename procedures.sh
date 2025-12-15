d79aed1503d9462ea390cfa97b03e8eab24ccbee
Jakub Janak <janakja5@fel.cvut.cz>
2025-12-15 21:32:29 +0100

    added the second scenario

---
+++ procedures.sh
@@ -0,0 +1,38 @@
+send_to_one() {
+  local ID="$1"
+  local key="$2"
+
+  local NODE="${NODE_IP[$ID]}"
+  local SESSION="${SESSION_PREFIX}_${ID}"
+
+  echo "[NODE_${ID} @ ${NODE}] sending '${key}' to tmux session '${SESSION}'"
+
+  # Only send if the session exists
+  sshpass -p "${DSV_PASS}" ssh -o StrictHostKeyChecking=no "${REMOTE_USER}@${NODE}" -- \
+    "tmux has-session -t '${SESSION}' 2>/dev/null && tmux send-keys -t '${SESSION}' '${key}' Enter || true"
+}
+
+send_to_all() {
+  local key="$1"
+  local skip_last="${2:-0}"   # how many nodes at the end to skip (default 0)
+
+  local last=$((NUM_NODES - skip_last))
+  if (( last < 1 )); then
+    echo "send_to_all: skip_last=${skip_last} skips everything (NUM_NODES=${NUM_NODES})"
+    return 0
+  fi
+
+  for ID in $(seq 1 "${last}") ; do
+    send_to_one "$ID" "$key"
+  done
+}
+
+restart_node_from_tmux_history() {
+  local ID="$1"
+  local NODE="${NODE_IP[$ID]}"
+  local SESSION="${SESSION_PREFIX}_${ID}"
+
+  echo "[NODE_${ID} @ ${NODE}] restart via Up+Enter in tmux '${SESSION}'"
+  sshpass -p "${DSV_PASS}" ssh -o StrictHostKeyChecking=no "${REMOTE_USER}@${NODE}" -- \
+    "tmux has-session -t '${SESSION}' 2>/dev/null && tmux send-keys -t '${SESSION}' Up Enter || true"
+}
---
+++ scenario-2.sh
@@ -0,0 +1,18 @@
+source ./procedures.sh
+
+# one instance enters the critical state first
+send_to_one 5 "e"
+sleep 1
+
+# 3 other instances start running - they wait between the CS requests
+send_to_one 1 "e"
+sleep 1
+send_to_one 2 "e"
+sleep 1
+send_to_one 3 "e"
+
+# the first instance holding releases CS
+sleep 1
+send_to_one 5 "l"
+
+# todo you have to release other nodes
