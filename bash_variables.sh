8d127fea6c64cf1d9036bc71d9049447835d082a
Jakub Janak <janakja5@fel.cvut.cz>
2025-12-15 20:15:23 +0100

    starting 5 nodes

--- bash_variables.sh
+++ bash_variables.sh
@@ -9,16 +9,20 @@ GIT_URL="git@github.com:jakubjanak2004/carvalho-roucairol.git"
 GIT_BRANCH="master"
 
 # Nodes
-NUM_NODES=3
+NUM_NODES=5
 declare -a NODE_IP
 NODE_IP[1]="192.168.1.100"
 NODE_IP[2]="192.168.1.101"
 NODE_IP[3]="192.168.1.102"
+NODE_IP[4]="192.168.1.101"
+NODE_IP[5]="192.168.1.102"
 
 declare -a NODE_PORT
 NODE_PORT[1]="12345"
 NODE_PORT[2]="12345"
 NODE_PORT[3]="12345"
+NODE_PORT[4]="12400"
+NODE_PORT[5]="12400"
 
 # SSH login (CTU lab often uses dsv/dsv, your case is dsva)
 REMOTE_USER="dsva"
